40003fdeddb16ea3652f5d63d0b81ad43941c26d
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-05-20 11:44:42 +0200

    node kommentare

--- intro-node-modules/index.js
+++ intro-node-modules/index.js
@@ -2,12 +2,16 @@ console.log('Intro file running');
 
 console.log('Veränderung');
 
+// Node hat Zugriff auf andere Objekte und Methoden als JavaScript im Browser. So können wir beispielsweise Umgebungsvariablen direkt auslesen oder auf das Dateisystem zugreifen. Andere Features sind stattdessen nicht vorhanden. document.getElementById("someId") würde einen Fehler aufwerfen.
 console.log(process.env.MY_ENV);
 
+// Viele Features von Node findet ihr in eingebauten Paketen. 'fs' enthält Methoden um z.B. Dateien zu schreiben oder zu lesen.
 // import { writeFileSync, writeFile } from 'node:fs';
 
+// Die synchrone Varianten (Sync) ist nicht das übliche Vorgehen in Node. Sie blockieren den Main Thread und damit die weitere Ausführung des Programms. Aber sie haben auch dann und wann ihren Platz.
 // writeFileSync('meineDatei.txt', 'Hallo Datei!');
 
+// Typischer Node-Code ist asynchron. Eine Aufgabe wird an andere Prozesse delegiert. Node ist hier wie ein Manager und verteilt Aufgaben. Klassischerweise geben wir eine Callback-Funktion mit. Wenn die Aufgabe erledigt ist, wird der Callback aufgerufen und gibt eventuelle Fehler oder die zurückgegebenen Daten an den Main Thread zurück.
 // writeFile('meineAsyncDatei.txt', 'Test from async', (error) => {
 //   if (error) {
 //     console.log(error);
@@ -18,13 +22,20 @@ console.log(process.env.MY_ENV);
 //   }
 // });
 
+// Wichtige Node Module haben heute auch eine Variante mit Promise-Syntax. Sie müssen von 'fs/promises' importiert werden.
 import { writeFile, readFile } from 'node:fs/promises';
 
+// Promises ermöglicht euch mit dem vertrauten async/await-Syntax zu arbeiten.
 try {
   await writeFile('writtenInPromises.txt', 'Wurde mit modernem Promise-Syntax erstellt.');
 } catch (error) {
   console.log('there was an error: ', error);
 }
+
+// Dies ist eine andere Schreibweise, der .then()-Syntax.
+// writeFile('writtenInPromises.txt', 'Wurde mit modernem Promise-Syntax erstellt.')
+//   .then((data) => console.log('File written'))
+//   .catch((err) => console.log('there was an error: ', err));
 
 let fileContent;
 
@@ -33,5 +44,10 @@ try {
 } catch (error) {
   console.log('there was an error: ', error);
 }
+
+// .then() ist ebenso Promise-basiert. Es ist nur eine andere Schreibweise.
+// readFile('writtenInPromises.txt', 'utf8')
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log('there was an error: ', err));
 
 console.log(fileContent);
--- intro-node-modules/server.js
+++ intro-node-modules/server.js
@@ -1,10 +1,14 @@
 import http from 'node:http';
 
+// Das http-Modul enhält all die low-level-Implementationen für einen HTTP-Server. Der Server muss Netzwerkverbindungen aufbauen und stabil auf Anfragen warten. Wenn ihr neugierig seid, wie das funktioniert, schaut in ein paar Monaten mal hier vorbei: https://app.codecrafters.io/courses/http-server/overview
 http
   .createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/html');
-    response.end(`
+    // Callback hat Zugriff auf die aktuelle Anfrage und die Anwort, die geschickt wird
+    response.statusCode = 200; // Eine HTTP-Antwort muss einen Status Code senden (https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status)
+    response.setHeader('Content-Type', 'text/html'); // Dieser Header teilt dem Browser mit, wie er die Daten interpretieren soll. Ändert das mal zu 'text/plain' und geht wieder auf http://localhost:8000
+
+    // Jede Anfrage muss beendet werden.
+    response.end(` 
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,4 +36,4 @@ http
 </html>
       `);
   })
-  .listen(8000);
+  .listen(8000); // Dies ist im Grunde eine lang laufende "while"-Schleife, die auf eingehende Anfragen lauscht.

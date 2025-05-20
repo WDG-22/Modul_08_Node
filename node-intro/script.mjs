3de169b29e70c38dd6873ca09dac6088efc62aab
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-05-20 10:48:03 +0200

    node intro

---
+++ node-intro/script.mjs
@@ -0,0 +1,10 @@
+// console.log(document.getElementById('someId'));
+// console.log(window);
+
+// console.log(process.env);
+
+// const { add } = require('./utils.js');
+
+import { add } from './utils.mjs';
+
+console.log(add(12, 45));
